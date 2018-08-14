cdb26adf1f04fe738deb6bc059899d4d2f56c7bd
Artur Szymczyk <arturszymczyk1@gmail.com>
2018-08-14 10:28:04 +0200

    Rebuilds 404 page

--- assets/js/components/Notfound.vue
+++ assets/js/components/Notfound.vue
@@ -1,16 +1,18 @@
 <template>
-    <div>
-        <template slot="header">
-            404 Not Found
-        </template>
-        <template slot="lead">
-            Looking for something? It seems that you were looking for something.
-        </template>
-        <hr class="my-4">
-        <p>
-            We didn't found content that you've been loking for. Check if you got correct link in your address bar.
-        </p>
-    </div>
+    <section id="not-found" class="main">
+        <div class="container">
+            <article class="message is-danger">
+                <div class="message-header">
+                    <p>404 Not Found</p>
+                </div>
+                <div class="message-body">
+                    <p>Looking for something? It seems that you were looking for <strong>something</strong>.</p>
+                    <p>We didn't found content that you've been loking for. Check if you got correct link in your address bar.</p>
+                </div>
+            </article>
+            <hr>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -20,11 +22,5 @@
 </script>
 
 <style scoped>
-    b-jumbotron {
-        background-size: 100% 100%;
-        padding-top: 20%;
-        padding-bottom: 20%;
-        height: calc(100%);
-    }
 
 </style>
